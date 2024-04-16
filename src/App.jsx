dfb6c78dc9e050210b3a437ae9f01f0889e2bb6a
EmySim <emiliesimson@gmail.com>
2024-04-16 14:17:58 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Navbar from './components/Navbar.jsx'; 
+import Header from './components/Header.jsx'; 
+import Footer from './components/Footer.jsx';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <Navbar />
+      <Header />
+      <Footer />
     </div>
   );
 }
