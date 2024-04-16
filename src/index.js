c14dec09b554a9d2c710ece2dd5af14301007afe
EmySim <emiliesimson@gmail.com>
2024-04-16 15:21:01 +0200

    mise en place router

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css';
 import App from './App.jsx';
 import reportWebVitals from './reportWebVitals';
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+  },
+  {
+    path: "/hello",
+    element: <div>hello world (ﾉ◕ヮ◕)ﾉ*:・ﾟ✧</div>
+  }
+]);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
