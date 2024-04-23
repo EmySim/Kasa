b9125a52edea2c46498c2cbae1a2ea73a8e931f9
EmySim <emiliesimson@gmail.com>
2024-04-23 14:33:27 +0200

    fix css sur logement et banner

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import './index.css';
-import routes from './pages/Route';
-
+import "./index.css";
+import routes from "./pages/Route";
 
 const router = createBrowserRouter(routes);
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
