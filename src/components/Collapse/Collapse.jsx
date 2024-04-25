44d11ec9f1c7f190b311e8f127ab203bb4635b03
EmySim <emiliesimson@gmail.com>
2024-04-25 16:16:20 +0200

    tentative collapse

--- src/components/Collapse/Collapse.jsx
+++ src/components/Collapse/Collapse.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./Collapse.css";
 import arrowCollapse from "../../assets/arrowCollapse.png";
 
-function Collapse({ title, content, width }) {
+function Collapse({ title, content }) {
   const [isCollapsed, setIsCollapsed] = useState(false);
 
   const toggleCollapse = () => {
@@ -10,18 +10,29 @@ function Collapse({ title, content, width }) {
   };
 
   return (
-    <div className="collapse" style={{ width: width }}>
-      <div  className="topbar">
-      {title}
-        <img 
-        src={arrowCollapse} 
-        alt="Chevron"onClick={toggleCollapse}  
-        className={`collapse-icon ${isCollapsed ? "collapsed" : ""}`} 
+
+    <div className="collapse">
+
+      <div className="topbar">
+        {title}
+        <img
+          src={arrowCollapse}
+          alt="fleche"
+          onClick={toggleCollapse}
+          className={`collapse-icon ${isCollapsed ? "collapsed" : ""}`}
         />
       </div>
-      {isCollapsed && <div className="collapse-content">{content}</div>}
+
+      {isCollapsed && (
+        <div className={`collapse-content ${isCollapsed ? "" : "hidden"}`}>
+          {content}
+        </div>
+      )}
+    
     </div>
+
   );
 }
+
 
 export default Collapse;
