2fdf3393dfc7582ca1e50a6b6e4a10e48c0bfbbb
EmySim <emiliesimson@gmail.com>
2024-04-26 00:57:51 +0200

    responsive v1

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,13 @@
-import React, { useEffect } from 'react';
-import './Home.css';
-import Header from '../components/Header/Header.jsx'; 
-import Footer from '../components/Footer/Footer.jsx';
-import Banner from '../components/Banner/Banner.jsx';
-import bannerHome from '../assets/bannerHome.jpg';
-import Card from '../components/Card/Card.jsx'
-import logementsData from '../data/logements.json';
+import React, { useEffect } from "react";
+import "./Home.css";
+import Header from "../components/Header/Header.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import Banner from "../components/Banner/Banner.jsx";
+import bannerHome from "../assets/bannerHome.jpg";
+import Card from "../components/Card/Card.jsx";
+import logementsData from "../data/logements.json";
 
 function Home() {
-
   useEffect(() => {
     // Utilisation de console.log pour afficher les données du fichier logements.json
     console.log(logementsData);
@@ -20,12 +19,21 @@ function Home() {
   return (
     <div>
       <Header />
-      <Banner backgroundImage={bannerHome} label="Chez vous, partout et ailleurs" />
-      <div className='card-container'>
-      {logementsData.map((logement, index) => (
-          <Card key={index} title={logement.title} id={logement.id} cover={logement.cover} />
+      <Banner
+        backgroundImage={bannerHome}
+        label="Chez vous, partout et ailleurs"
+      />
+      <div className="card-container">
+        {logementsData.map((logement, index) => (
+          <Card
+            key={index}
+            title={logement.title}
+            id={logement.id}
+            cover={logement.cover}
+          />
         ))}
       </div>
+
       <Footer />
     </div>
   );
