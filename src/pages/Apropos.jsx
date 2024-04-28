a3100668e0253f45def970bcaa31d692a0ad7e2c
EmySim <emiliesimson@gmail.com>
2024-04-28 03:12:02 +0200

    scss/responsive/add feature gallery

---
+++ src/pages/Apropos.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import Header from "../components/Header/Header.jsx";
+import Banner from "../components/Banner/Banner.jsx";
+import bannerApropos from "../assets/bannerApropos.jpg";
+import Collapse from "../components/Collapse/Collapse.jsx";
+import Footer from "../components/Footer/Footer.jsx";
+import valeurs from "../data/valeurs.json";
+import "./Apropos.css";
+
+function Apropos() {
+  return (
+    <div>
+      <Header />
+      {<Banner backgroundImage={bannerApropos} />}
+      <div className="collapse-container">
+        {valeurs.map((valeur, index) => (
+          <Collapse key={index} title={valeur.title} content={valeur.content} />
+        ))}
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+export default Apropos;
